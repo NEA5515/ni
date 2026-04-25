12eee3e388aef791fdd2f5e9b3441f9a39f30955
Niclas Hartung <deine-email@example.com>
2026-04-25 22:30:38 +0200

    rename title

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,24 +3,24 @@ import "./globals.css";
 
 export const metadata: Metadata = {
   metadataBase: new URL("https://ni-two-kappa.vercel.app"),
-  title: "Niclas Lernstandstest Grundrechenarten",
+  title: "Niclas Lernkontrolle Grundrechenarten",
   description: "Teste dein Können in Addition, Subtraktion, Multiplikation und Division.",
   openGraph: {
-    title: "Niclas Lernstandstest Grundrechenarten",
-    description: "Interaktiver Lernstandstest für Kinder.",
+    title: "Niclas Lernkontrolle Grundrechenarten",
+    description: "Interaktiver Test für Grundschüler.",
     images: [
       {
         url: "https://ni-two-kappa.vercel.app/preview-v2.jpg",
         width: 1200,
         height: 630,
-        alt: "Niclas Lernstandstest Grundrechenarten",
+        alt: "Niclas Lernkontrolle Grundrechenarten",
       },
     ],
     type: "website",
   },
   twitter: {
     card: "summary_large_image",
-    title: "Niclas Lernstandstest Grundrechenarten",
+    title: "Niclas Lernkontrolle Grundrechenarten",
     description: "Teste dein Können in den Grundrechenarten.",
     images: ["https://ni-two-kappa.vercel.app/preview-v2.jpg"],
   },
